a15cdc06708d803eb62d1562f429d74372a9155b
jinotter3 <jinotter3@snu.ac.kr>
2023-09-02 03:42:27 +0900

    project structure change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -7,7 +8,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  runApp(
+    ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
---
+++ lib/models/student.dart
@@ -0,0 +1,11 @@
+class Student {
+  final String name;
+  final String studentId;
+  final int id;
+
+  Student({
+    required this.name,
+    required this.studentId,
+    required this.id,
+  });
+}
